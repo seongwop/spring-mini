0e8d946f21f79eedcb5c6ee8eec1bd447dc67421
seongwop <psw921@gmail.com>
2026-04-04 00:05:32 +0900

    feat: apply fetch join to order pagination query

--- src/main/java/org/example/mini/order/OrderRepository.java
+++ src/main/java/org/example/mini/order/OrderRepository.java
@@ -1,6 +1,12 @@
 package org.example.mini.order;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query(value = "select o from Order o join fetch o.product")
+    Page<Order> findAll(Pageable pageable);
 }
